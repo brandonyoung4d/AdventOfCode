446a69eeb6fe891b4a3486785727d2ba9be93bb8
Brandon Young <brandonyoung4d@gmail.com>
2025-12-05 11:14:14 -0800

    2025 day 1 through day 4

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  AdventOfCode2025
+//
+//  Created by Brandon Young on 2025-12-02.
+//
+
+import Foundation
+
+print("Hello, Advent of Code 2025!")
+
+//print("Day 1 answer = \(day1())")
+//print("Day 2 answer = \(day2())")
+//print("Day 3 answer = \(day3())")
+//print("Day 3 Part 2 answer = \(day3part2())")
+//print("D4 P1 answer = \(day4Part1())")
+print("D4 P2 answer = \(day4Part2())")
