3005fa7125662d874865836e8d0f115b0af492e1
Brandon Young <brandonyoung4d@gmail.com>
2025-12-07 16:58:43 -0800

    Day 6 and 7

--- AdventOfCode/main.swift
+++ AdventOfCode/main.swift
@@ -9,10 +9,4 @@ import Foundation
 
 print("Hello, Advent of Code 2025!")
 
-//print("Day 1 answer = \(day1())")
-//print("Day 2 answer = \(day2())")
-//print("Day 3 answer = \(day3())")
-//print("Day 3 Part 2 answer = \(day3part2())")
-//print("D4 P1 answer = \(day4Part1())")
-//print("D4 P2 answer = \(day4Part2())")
-print("D5 P2 answer = \(day5Part2())")
+year2026Day7Part2()
